6443a2f8775935c7772d922f75e820a9057cba6c
coldmoon6261 <coldmoon6261@163.com>
2019-10-29 18:00:00 +0800

    Chapter 11 - 'User Authentification with Django-Registration-Redux' finished.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -84,66 +84,66 @@ def add_page(request, category_name_slug):
     context_dict = {'form':form, 'category':category}
     return render(request, 'rango/add_page.html', context_dict)
 
-def register(request):
-    registered = False
+# def register(request):
+#     registered = False
     
-    if request.method == 'POST':
-        uesr_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
+#     if request.method == 'POST':
+#         uesr_form = UserForm(data=request.POST)
+#         profile_form = UserProfileForm(data=request.POST)
 
-        if uesr_form.is_valid() and profile_form.is_valid():
-            user = uesr_form.save()
-            user.set_password(user.password)
-            user.save()
+#         if uesr_form.is_valid() and profile_form.is_valid():
+#             user = uesr_form.save()
+#             user.set_password(user.password)
+#             user.save()
             
-            profile = profile_form.save(commit=False)
-            profile.user = user
+#             profile = profile_form.save(commit=False)
+#             profile.user = user
 
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-            profile.save()
+#             if 'picture' in request.FILES:
+#                 profile.picture = request.FILES['picture']
+#             profile.save()
 
-            registered = True
-        else:
-            print(uesr_form.errors, profile_form.errors)
-    else:
-        uesr_form = UserForm()
-        profile_form = UserProfileForm()
+#             registered = True
+#         else:
+#             print(uesr_form.errors, profile_form.errors)
+#     else:
+#         uesr_form = UserForm()
+#         profile_form = UserProfileForm()
 
-    return render(request,
-                  'rango/register.html',
-                  {'user_form':uesr_form,
-                   'profile_form':profile_form,
-                   'registered':registered})
+#     return render(request,
+#                   'rango/register.html',
+#                   {'user_form':uesr_form,
+#                    'profile_form':profile_form,
+#                    'registered':registered})
 
-def uesr_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+# def uesr_login(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
 
-        user = authenticate(username=username,password=password)
+#         user = authenticate(username=username,password=password)
 
-        if user:
-            if user.is_active:
-                login(request, user)
-                return redirect(reverse('rango:index'))
-            else:
-                return HttpResponse('Your Rango account is disabled.')
-        else:
-            print('Invalid login details:{0}, {1}'.format(username, password))
-            return HttpResponse('Invalid login details supplied.')
+#         if user:
+#             if user.is_active:
+#                 login(request, user)
+#                 return redirect(reverse('rango:index'))
+#             else:
+#                 return HttpResponse('Your Rango account is disabled.')
+#         else:
+#             print('Invalid login details:{0}, {1}'.format(username, password))
+#             return HttpResponse('Invalid login details supplied.')
     
-    else:
-        return render(request, 'rango/login.html')
+#     else:
+#         return render(request, 'rango/login.html')
 
 @login_required
 def resricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
 
-@login_required
-def user_logout(request):
-    logout(request)
-    return redirect(reverse('rango:index'))
+# @login_required
+# def user_logout(request):
+#     logout(request)
+#     return redirect(reverse('rango:index'))
 
 def get_server_side_cookie(request, cookie, default_val=None):
     val = request.session.get(cookie)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rango',
     'polls.apps.PollsConfig',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,17 @@ MEDIA_URL = '/media/'
 
 #Login
 LOGIN_URL = 'rango:login'
+
+# Registration
+# If True, users can register.
+REGISTRATION_OPEN=True
+
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN=True
+
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL='rango:index'
+
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL='auth_login'
